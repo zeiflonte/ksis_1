4427b77ad67dc99c0e29dab6803fcbba8b998767
zeiflonte <zeiflonte@gmail.com>
2018-03-11 20:55:55 +0300

    Trash has been deleted

--- ConsoleApplication1/ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/ConsoleApplication1/Source.cpp
@@ -23,47 +23,11 @@ ASTAT Adapter;
 // Функция получения MAC адреса.
 // На вход получает указатель на буфер, куда записывается строковое
 // представление полученного MAC адреса.
-void GetMacAddress1()
-{
-    NCB ncb;
-    UCHAR uRetCode;
-    //char NetName[50];
-    memset(&ncb, 0, sizeof(ncb));
-    ncb.ncb_command = NCBRESET;
-    ncb.ncb_lana_num = 0;
-
-    uRetCode = Netbios(&ncb);
-
-    memset(&ncb, 0, sizeof(ncb));
-    ncb.ncb_command = NCBASTAT;
-    ncb.ncb_lana_num = 0;
-
-    strcpy((char *)ncb.ncb_callname, "* ");
-    ncb.ncb_buffer = (unsigned char *)&Adapter;
-    ncb.ncb_length = sizeof(Adapter);
-
-    uRetCode = Netbios(&ncb);
-    if (uRetCode == 0)
-    {
-        printf("The Ethernet Number is: %02X-%02X-%02X-%02X-%02X-%02X\n",
-            Adapter.adapt.adapter_address[0],
-            Adapter.adapt.adapter_address[1],
-            Adapter.adapt.adapter_address[2],
-            Adapter.adapt.adapter_address[3],
-            Adapter.adapt.adapter_address[4],
-            Adapter.adapt.adapter_address[5]);
-    }
-    else
-    {
-        printf("uRetCode == %d\n", uRetCode);
-    }
-}
 
 void GetMacAddress()
 {
     NCB Ncb;
     UCHAR uRetCode;
-   // char NetName[50];
     LANA_ENUM lenum;
     int i;
 
